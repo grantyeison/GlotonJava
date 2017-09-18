c12b598ced46bfd57b22ac2626e84a36a087570e
Apollo <Apollo@DESKTOP-7J1DE59>
2017-09-18 11:51:07 -0500

    creación primer bean con mejoras

--- src/java/Bean/CalificacionBean.java
+++ src/java/Bean/CalificacionBean.java
@@ -6,6 +6,7 @@
 package Bean;
 import DAO.CalificacionDAO;
 import DAO.CalificacionJPADAO;
+import DAO.PlatoRestauranteJPADAO;
 import Modelo.Calificacion;
 import Modelo.Plato;
 import Modelo.PlatoRestaurante;
@@ -38,6 +39,47 @@ public class CalificacionBean
             {
                 califPlato.add(cal);
             }
+        }
+        return califPlato;
+    }
+    
+    public List<Calificacion> GenerarReporteRestaurante(Restaurante rest)
+    {
+        List<Calificacion> calificaciones = dao.ListarCalificacion();
+        List<Calificacion> califPlato = new LinkedList<>();
+        for (Calificacion cal : calificaciones)
+        {
+            if (cal.getTblPlatoRestauranteplatId().getTblRestauranteresid() == rest)
+            {
+                califPlato.add(cal);
+            }
+        }
+        return califPlato;
+    }
+    
+    public List<Calificacion> GenerarReporteCalificaciones()
+    {
+        List<Calificacion> calificaciones = dao.ListarCalificacion();
+        List<Calificacion> califPlato = new LinkedList<>();
+        List<PlatoRestaurante> platos = new PlatoRestauranteJPADAO().ListarPlatoRestaurante();
+        for (PlatoRestaurante plato : platos)
+        {
+            int suma = 0;
+            int votantes = 0;
+            int promedio = 0;
+            for (Calificacion cal : calificaciones)
+            {
+                if (cal.getTblPlatoRestauranteplatId() == plato)
+                {
+                    suma += cal.getCalPuntuacion();
+                    votantes ++;
+                }
+            }
+            promedio = suma/votantes;
+            Calificacion calif = new Calificacion();
+            calif.setTblPlatoRestauranteplatId(plato);
+            calif.setCalPuntuacion(promedio);
+            califPlato.add(calif);
         }
         return califPlato;
     }
